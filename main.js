6f25a5df7e227345d7ca3a75bae5af994bf1c6b6
Teeshk Nader <90448303+hello-suprice@users.noreply.github.com>
2022-05-03 23:41:26 +0200

    Hmm, fixa under

--- main.js
+++ main.js
@@ -15,11 +15,14 @@ let gameSpeed = 2;
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     //ctx.fillRect(10, canvas.height - 90, 50, 50);
+    handleObstacles();
     bird.update();
     bird.draw();
     //handleParticles();
+    
     requestAnimationFrame(animate);
     angle+=0.12;
+    frame++; 
 
 }
 animate();
---
+++ obstacles.js
@@ -0,0 +1,33 @@
+const obstaclesArray = [];
+
+class Obstacle{
+    constructor(){
+        this.top = (Math.random() * canvas.height/3) + 20;
+        this.buttom = (Math.random() * canvas.height/3) + 20;
+        this.x = canvas.width;
+        this.width = 20;
+        this.color = 'black'
+    }
+    draw(){
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, 0, this.width, this.top);
+        ctx.fillRect(this.x, canvas.height - this.bottom, this.width, this.bottom);
+
+    }
+    update(){
+        this.x -= gameSpeed;
+        this.draw();
+    }
+}
+
+function handleObstacles(){
+    if (frame%100 ===0) {
+        obstaclesArray.unshift(new Obstacle);
+    }
+    for (let i = 0; i < obstaclesArray.length; i++){
+        obstaclesArray[i].update();
+    }
+    if (obstaclesArray.length > 20){
+        obstaclesArray.pop(obstaclesArray[0]);
+    }
+}
