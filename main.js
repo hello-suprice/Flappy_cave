b568dceb62291e75b8a037219e71abe8b62b3342
Teeshk Nader <teeshk.m@gmail.com>
2022-05-11 14:45:58 +0200

    Well

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ canvas.height = 400;
 
 let spacePressed = false;
 let angle = 0;
-//let hue = 0;
+     
 let frame = 0;
 let score = 0;
 let gameSpeed = 2;
@@ -34,3 +34,25 @@ window.addEventListener('keydown', function(e) {
 window.addEventListener('keyup', function(e) {
     if (e.code === 'Space') spacePressed = false;
 });
+
+const bang = new Image();
+bang.src = 'bang.png';
+function handleCollisions(){
+    for (let i = 0; i < obstaclesArray.length; i++){
+        if (bird.x < obstaclesArray[i].x + obstaclesArray[i].width &&
+            bird.x + bird.width > obstaclesArray[i].x &&
+            ((bird.y < 0 + obstaclesArray[i].top && bird.y + bird.height > 0) || 
+            (bird.y > canvas.height - obstaclesArray[i].bottom && 
+            bird.y + bird.height < canvas.height))) {
+                // kollision hittad
+                ctx.drawImage(bang, bird.x, bird.y, 50, 50);
+                ctx.front = "25px Georgia";
+                ctx.fillStyle = 'black';
+                ctx.fillText('Game Over nub and here is your score' + score, 160, canvas.height/2 - 10);
+
+                return true;
+            }
+    }
+}
+
+
