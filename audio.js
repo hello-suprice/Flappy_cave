d7a205551d073920d7fcf6a4a61dfb74d87f22ba
GasMaster69 <92572610+GasMaster69@users.noreply.github.com>
2022-05-19 15:57:16 +0200

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,11 +1,10 @@
-function playMusic() {
-    var x = document.getElementById("audio2").autoplay;
-    document.getElementById("demo").innerHTML = x;
-}
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 65) {
+      document.getElementById('audio2').play();
+    }
+  });
 
 function playExplosion() {
     var audio1 = new Audio('mixkit-sea-mine-explosion-1184.wav');
     audio1.play();
 }
-
-playMusic();
